20335cea3724fbcfb3891054e1580fc63bcabfe8
freitim <frei.timothy@gmail.com>
2024-01-14 22:12:15 +0100

    update config

--- config/default.nix
+++ config/default.nix
@@ -3,16 +3,63 @@
   imports = [
     ./bufferline.nix
   ];
-
-  # options = {
-  #   number = true;
-  # };
-
-  colorschemes.rose-pine.enable = true;
   
-  plugins = {
-    lightline = {
+  config = {
+    options = {
+      number = true;
+      shiftwidth = 4;
+    };
+
+    colorschemes.rose-pine = {
       enable = true;
+      style = "dawn";
+    };
+    
+    plugins = {
+      lualine = {
+        enable = true;
+      };
+
+      lsp = {
+        enable = true;
+
+        servers = {
+	  digestif = {
+            enable = true;
+	  };
+
+          html = {
+            enable = true;
+	  };
+          
+          julials = {
+            enable = true;
+	  };
+
+	  # leanls = {
+          #   enable = true;
+	  # };
+
+          lua-ls = {
+            enable = true;
+	  };
+
+          marksman = {
+            enable = true;
+	  };
+
+          nil_ls = {
+            enable = true;
+	  };
+
+          pylsp = {
+            enable = true;
+	    settings.plugins.black = {
+              enabled = true;
+	    };
+	  };
+	};
+      };
     };
   };
 }
