a7634d2d3296fa5933226f1ae2090e6f508f835d
freitim <frei.timothy@gmail.com>
2025-04-04 12:04:00 +0200

    Update config

--- modules/utilities/completion.nix
+++ modules/utilities/completion.nix
@@ -1,49 +1,54 @@
-{ ... }:
-
-{
+{...}: {
   plugins = {
-		cmp-latex-symbols = {
-			enable = true;
-		};
+    cmp-latex-symbols = {
+      enable = true;
+    };
 
-		blink-compat = {
-			enable = true;
-		};
+    blink-compat = {
+      enable = true;
+    };
 
-		blink-cmp = {
-			enable = true;
-			settings = {
-				keymap = {
-					preset = "default";
-					"<C-k>" = [
-						"select_prev"
-						"fallback"
-					];
-					"<C-j>" = [
-						"select_next"
-						"fallback"
-					];
-					"<Tab>" = [
-						"select_and_accept"
-						"fallback"
-					];
-				};
-				sources = {
-					default = [
-						"lsp"
-						"path"
-						"snippets"
-						"buffer"
-						"symbols"
-					];
-					providers = {
-						symbols = {
-							name = "latex_symbols";
-							module = "blink.compat.source";
-						};
-					};
-				};
-			};
-		};
+    blink-cmp = {
+      enable = true;
+      settings = {
+        keymap = {
+          preset = "default";
+          "<C-k>" = [
+            "select_prev"
+            "fallback"
+          ];
+          "<C-j>" = [
+            "select_next"
+            "fallback"
+          ];
+          "<Tab>" = [
+            "select_and_accept"
+            "fallback"
+          ];
+        };
+        cmdline = {
+          completion = {
+            menu.auto_show = true;
+            ghost_text.enabled = true;
+          };
+          keymap.preset = "inherit";
+        };
+        sources = {
+          default = [
+            "lsp"
+            "path"
+            "snippets"
+            "buffer"
+            "symbols"
+          ];
+          providers = {
+            symbols = {
+              name = "latex_symbols";
+              module = "blink.compat.source";
+            };
+          };
+        };
+      };
+    };
   };
 }
