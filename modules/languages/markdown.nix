4c3f96f3c646669414ce2ca5ea3531d51aa05ed5
freitim <frei.timothy@gmail.com>
2025-04-07 20:44:55 +0200

    Update config

--- modules/languages/markdown.nix
+++ modules/languages/markdown.nix
@@ -1,4 +1,7 @@
-{...}: {
+{pkgs, ...}: {
+  extraPackages = with pkgs; [
+    python3Packages.pylatexenc
+  ];
   plugins = {
     render-markdown = {
       enable = true;
@@ -6,7 +9,12 @@
         preset = "obsidian";
         file_types = ["markdown" "quarto"];
         completions.blink.enabled = true;
-        latex.enabled = true;
+        latex = {
+          enabled = true;
+          render_modes = false;
+          converter = "latex2text";
+          highlight = "RenderMarkdownMath";
+        };
       };
     };
   };
