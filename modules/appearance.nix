d211fdc928345f8309d8fa33ddcc474800844995
freitim <frei.timothy@gmail.com>
2025-04-22 21:55:54 +0200

    Update flake

--- modules/appearance.nix
+++ modules/appearance.nix
@@ -1,60 +1,58 @@
-{ pkgs, ... }:
+{pkgs, ...}: {
+  # colorschemes = {
+  # 	everforest = {
+  # 		enable = true;
+  # 	};
+  # 	# base16 = {
+  # 	# 	enable = true;
+  # 	# 	colorscheme = "everforest";
+  # 	# };
+  # };
 
-{
-	# colorschemes = {
-	# 	everforest = {
-	# 		enable = true;
-	# 	};
-	# 	# base16 = {
-	# 	# 	enable = true;
-	# 	# 	colorscheme = "everforest";
-	# 	# };
-	# };
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "evergarden";
+      src = pkgs.fetchFromGitHub {
+        owner = "comfysage";
+        repo = "evergarden";
+        rev = "mega";
+        hash = "sha256-ZCw+dg+Z1+tDBPhj9/qbJZpyqovU9RcNtlV2oMjGfME=";
+      };
+      nvimSkipModule = "evergarden.extras";
+    })
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "aki";
+      src = pkgs.fetchFromGitHub {
+        owner = "comfysage";
+        repo = "aki";
+        rev = "mega";
+        hash = "sha256-lzyf6EtM6hOEdYwTrEBjpepy2eXKZz5meCkHiviBzGs=";
+      };
+      nvimSkipModule = "aki_test";
+    })
+  ];
 
-	extraPlugins = [
-		(pkgs.vimUtils.buildVimPlugin {
-			name = "evergarden";
-			src = pkgs.fetchFromGitHub {
-				owner = "comfysage";
-				repo = "evergarden";
-				rev = "mega";
-				hash = "sha256-nupDWpBE7TaDTUG11egCPq1SyLhSbjho9vEdwTYcywM=";
-			};
-			nvimSkipModule = "evergarden.extras";
-		})
-		(pkgs.vimUtils.buildVimPlugin {
-			name = "aki";
-			src = pkgs.fetchFromGitHub {
-				owner = "comfysage";
-				repo = "aki";
-				rev = "mega";
-				hash = "sha256-lzyf6EtM6hOEdYwTrEBjpepy2eXKZz5meCkHiviBzGs=";
-			};
-			nvimSkipModule = "aki_test";
-		})
-	];
+  extraConfigLuaPre = ''
+    require('evergarden').setup({
+    	theme = {
+    		variant = 'fall',
+    		accent = 'green',
+    	};
+    })
 
-	extraConfigLuaPre = ''
-		require('evergarden').setup({
-			theme = {
-				variant = 'fall',
-				accent = 'green',
-			};	
-		})
+    require('aki').setup({
+    	contrast_dark = 'soft',
+    })
+  '';
 
-		require('aki').setup({
-			contrast_dark = 'soft',
-		})
-	'';
-
-	colorscheme = "evergarden";
+  colorscheme = "evergarden";
 
   plugins = {
-		web-devicons = {
-			enable = true;
-		};
-    
-		lualine = {
+    web-devicons = {
+      enable = true;
+    };
+
+    lualine = {
       enable = true;
     };
 
